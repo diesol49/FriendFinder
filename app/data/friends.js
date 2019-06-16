25e96928200a18d61880b71a2fcb9f4f77f39cea
unknown <diesol42@gmail.com>
2019-06-15 23:20:37 -0700

    the other files got code in, but still need to finish

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,62 +2,66 @@
 // CREATE ARRAY OF PEOPLE TO MAKE FRIENDS WITH
 //====================
 
-let friends = [{
-    "name": "Thor",
-    "photo": "https://images.app.goo.gl/TxGwFxen5LRNvBBD6",
-    "answers": []
+//===========
+// I went ahead and used a random number generator online
+//===========
+
+let friendsList = [{
+    name: "Thor",
+    photo: "https://images.app.goo.gl/TxGwFxen5LRNvBBD6",
+    scores: [5, 1, 1, 4, 3, 4, 2, 1, 5, 4]
 },
 {
-    "name": "Hulk",
-    "photo": "https://images.app.goo.gl/mbiWBAPK2UKXiLJfA",
-    "answers": []   
+    name: "Hulk",
+    photo: "https://images.app.goo.gl/mbiWBAPK2UKXiLJfA",
+    scores: [4, 1, 3, 1, 5, 2, 5, 2, 5, 5]   
 },
 {
-    "name": "Drax",
-    "photo": "https://images.app.goo.gl/f8ukh1Z7nBvYi7nBA",
-    "answers": []
+    name: "Drax",
+    photo: "https://images.app.goo.gl/f8ukh1Z7nBvYi7nBA",
+    scores: [1, 5, 4, 5, 1, 2, 3, 2, 4, 4]
 },
 {
-    "name": "Groot",
-    "photo": "https://images.app.goo.gl/1SLh79fYC9aoWzP96",
-    "answers":[]
+    name: "Groot",
+    photo: "https://images.app.goo.gl/1SLh79fYC9aoWzP96",
+    scores: [2, 4, 5, 3, 1, 4, 3, 2, 2, 4]
 },
 {
-    "name": "Thanos",
-    "photo": "https://images.app.goo.gl/e5Cu7t68cK8p8Mdh7",
-    "answers":[]
+    name: "Thanos",
+    photo: "https://images.app.goo.gl/e5Cu7t68cK8p8Mdh7",
+    scores: [2, 2, 5, 3, 1, 5, 1, 4, 1, 3]
 },
 {
-    "name": "Spiderman",
-    "photo": "https://images.app.goo.gl/aHdMD7GhR56LM69B6",
-    "answers":[]
-}
-{
-    "name": "Wolverine",
-    "photo": "https://images.app.goo.gl/3m483wT9eMWwocd89",
-    "answers":[]
+    name: "Spiderman",
+    photo: "https://images.app.goo.gl/aHdMD7GhR56LM69B6",
+    scores: [4, 4, 4, 1, 1, 4, 4, 5, 2, 2]
 },
 {
-    "name": "Captain America",
-    "photo": "https://images.app.goo.gl/xgEPjGr6AD4izjud6",
-    "answers":[]
+    name: "Wolverine",
+    photo: "https://images.app.goo.gl/3m483wT9eMWwocd89",
+    scores: [1, 5, 2, 3, 2, 3, 3, 5, 2, 3]
 },
 {
-    "name": "Iron Man",
-    "photo": "https://images.app.goo.gl/vxZkjP1vTRZn4Uah9",
-    "answers":[]
+    name: "Captain America",
+    photo: "https://images.app.goo.gl/xgEPjGr6AD4izjud6",
+    scores: [2, 3, 4, 1, 2, 4, 3, 4, 4, 1]
 },
 {
-    "name": "Ant Man",
-    "photo": "https://images.app.goo.gl/pFUB3VBXgThfXiwz6",
-    "answers":[]
+    name: "Iron Man",
+    photo: "https://images.app.goo.gl/vxZkjP1vTRZn4Uah9",
+    scores: [1, 1, 4, 2, 5, 3, 5, 2, 5, 2]
 },
 {
-    "name": "Loki",
-    "photo": "https://images.app.goo.gl/Qnkeqw4vQy5KPG5R6",
-    "answers":[]
+    name: "Ant Man",
+    photo: "https://images.app.goo.gl/pFUB3VBXgThfXiwz6",
+    scores: [4, 5, 1, 5, 3, 2, 5, 5, 2, 4]
+},
+{
+    name: "Loki",
+    photo: "https://images.app.goo.gl/Qnkeqw4vQy5KPG5R6",
+    scores: [2, 2, 1, 1, 3, 4, 4, 3, 2, 5]
 }
 
-]
+];
 
-module.exports = friends;
+module.exports = friendsList;
