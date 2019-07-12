4d1f9d33400cb9aeacd8616e05ffdab5f1a02c2e
unknown <diesol42@gmail.com>
2019-07-12 10:30:16 -0700

    changed some lines & added some comments

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = function(app) {
+    // We set paths for our URL's
 
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "/../public/survey.html"));
